e83578c6710bce6be4109028b8cc9c32f65a8f45
drummonds <drummond05@proton.me>
2026-01-03 07:06:04 -0300

    feat: migration for users table

---
+++ src/database/migrations/1767429493482_create-users-table.ts
@@ -0,0 +1,43 @@
+import { type ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable("users", {
+    id: {
+      type: "UUID",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    email: {
+      type: "varchar(100)",
+      unique: true,
+      notNull: true,
+    },
+    name: {
+      type: "varchar(100)",
+      notNull: true,
+    },
+    password_hash: {
+      type: "varchar(255)",
+      notNull: true,
+    },
+    role: {
+      type: "varchar(15)",
+      default: `'USER'`,
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamp",
+      default: pgm.func("NOW()"),
+    },
+  });
+
+  pgm.createConstraint("users", "user_role_check", {
+    check: "role IN ('USER', 'ORGANIZER' ,'ADMIN')",
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable("users", { cascade: true });
+}
