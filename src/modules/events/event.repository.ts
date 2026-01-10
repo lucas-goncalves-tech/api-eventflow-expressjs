62a2a98da57de73a8ece483e120a14bdd0d373a1
drummonds <drummond05@proton.me>
2026-01-10 15:20:19 -0300

    feat: event repository totalPage, update and delete

--- src/modules/events/event.repository.ts
+++ src/modules/events/event.repository.ts
@@ -23,10 +23,96 @@ export class EventRepository {
           data.capacity,
         ]
       );
+      console.log(event.rows);
       return event.rows[0];
-    } catch (error) {
-      console.error("Erro ao criar evento:", error);
-      throw error;
+    } catch (err) {
+      console.error("Erro ao criar evento:", err);
+      throw err;
+    }
+  }
+
+  async findById(id: string) {
+    try {
+      const result = await this.pool.query(
+        `SELECT * FROM events WHERE id = $1 AND deleted_at = NULL`,
+        [id]
+      );
+      return result.rows[0] ?? null;
+    } catch (err) {
+      console.log("Não foi possivel Encontrar evento por ID", err);
+      throw err;
+    }
+  }
+
+  async FindMany(search?: string, limit: number = 10, page: number = 1) {
+    const offset = (page - 1) * limit;
+    const safeLimit = Math.min(limit, 100);
+    const searchLike = `%${search}%`;
+    const index = search ? 2 : 1;
+    let query = `SELECT *, COUNT(*) OVER () AS total FROM events WHERE deleted_at IS NULL`;
+    let result;
+    try {
+      if (search) {
+        query += ` AND title ILIKE $1`;
+      }
+
+      result = await this.pool.query(
+        query +
+          ` ORDER BY created_at DESC LIMIT $${index} OFFSET $${index + 1}`,
+        [searchLike, safeLimit, offset]
+      );
+      const total =
+        result.rows.length > 0 ? parseInt(result.rows[0].total, 10) : 0;
+      return {
+        data: result.rows.map(({ total, ...row }) => row) as IEvent[],
+        meta: {
+          total,
+          page,
+          limit,
+          totalPages: Math.ceil(total / limit),
+        },
+      };
+    } catch (err) {
+      console.log("NÂO foi possivel encontrar todos os eventos", err);
+      throw err;
+    }
+  }
+
+  async update(id: string, eventData: Partial<IEvent>) {
+    const toUpdate: string[] = [];
+    const values: unknown[] = [];
+    let i = 2;
+
+    for (const [key, value] of Object.entries(eventData)) {
+      if (key === undefined || value === undefined) continue;
+      toUpdate.push(`${key} = $${i++}`);
+      values.push(value);
+    }
+
+    if (toUpdate.length === 0) return null;
+
+    try {
+      const result = await this.pool.query(
+        `UPDATE events SET ${toUpdate.join(", ")} WHERE id = $1 RETURNING *`,
+        [id, ...values]
+      );
+      return (result.rows[0] as IEvent) ?? null;
+    } catch (err) {
+      console.error("Erro ao fazer updade de events ", err);
+      throw err;
+    }
+  }
+
+  async delete(id: string) {
+    try {
+      const result = await this.pool.query(
+        `UPDATE events SET deleted_at = NOW() WHERE id = $1`,
+        [id]
+      );
+      return result.rowCount ?? null;
+    } catch (err) {
+      console.error("Error em soft delete event ", err);
+      throw err;
     }
   }
 }
