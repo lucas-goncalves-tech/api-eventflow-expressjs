cec4a3d5844a63b40eb84dc34afdda9e81824ac7
drummonds <drummond05@proton.me>
2026-01-13 17:10:14 -0300

    chore: change migration from ts to js extension for more easily usage of package script

--- src/database/migrations/1767429493482_create-users-table.js
+++ src/database/migrations/1767429493482_create-users-table.js
@@ -1,8 +1,8 @@
-import { type ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+import { MigrationBuilder } from "node-pg-migrate";
 
-export const shorthands: ColumnDefinitions | undefined = undefined;
+export const shorthands = undefined;
 
-export async function up(pgm: MigrationBuilder): Promise<void> {
+export async function up(pgm) {
   pgm.createTable("users", {
     id: {
       type: "UUID",
@@ -38,6 +38,6 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
   });
 }
 
-export async function down(pgm: MigrationBuilder): Promise<void> {
+export async function down(pgm) {
   pgm.dropTable("users", { cascade: true });
 }
--- src/database/migrations/1767679836064_create-events-table.js
+++ src/database/migrations/1767679836064_create-events-table.js
@@ -1,8 +1,8 @@
-import { type ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+import { MigrationBuilder } from "node-pg-migrate";
 
-export const shorthands: ColumnDefinitions | undefined = undefined;
+export const shorthands = undefined;
 
-export async function up(pgm: MigrationBuilder): Promise<void> {
+export async function up(pgm) {
   pgm.createTable("events", {
     id: {
       type: "UUID",
@@ -53,6 +53,6 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
   });
 }
 
-export async function down(pgm: MigrationBuilder): Promise<void> {
+export async function down(pgm) {
   pgm.dropTable("events");
 }
