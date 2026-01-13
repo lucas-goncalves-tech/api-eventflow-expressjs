299c2273409f1fded43d4e949d028f6e5fe24591
drummonds <drummond05@proton.me>
2026-01-13 17:09:34 -0300

    chore: remove unused envs

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -3,8 +3,6 @@ import z from "zod";
 const enviroments = z.object({
   MAX_POOL: z.coerce.number().default(10),
   PORT: z.coerce.number().default(3333),
-  POSTGRES_USER: z.string(),
-  POSTGRES_DB: z.string(),
   DATABASE_URL: z.string(),
   NODE_ENV: z.enum(["development", "production"]),
 });
