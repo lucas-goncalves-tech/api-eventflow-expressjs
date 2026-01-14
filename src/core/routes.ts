c279068dd89c3e22806fbe916b4874def3c349f4
drummonds <drummond05@proton.me>
2026-01-14 14:36:14 -0300

    .

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -1,17 +1,22 @@
 import { Router } from "express";
 import { EventRoutes } from "../modules/events/event.routes";
 import { inject, injectable } from "tsyringe";
+import { AuthRoutes } from "../modules/auth/auth.routes";
 
 @injectable()
 export class Routes {
   private router: Router;
-  constructor(@inject(EventRoutes) private eventRoutes: EventRoutes) {
+  constructor(
+    @inject(EventRoutes) private eventRoutes: EventRoutes,
+    @inject(AuthRoutes) private authRoutes: AuthRoutes
+  ) {
     this.router = Router();
     this.setupRoutes();
   }
 
   private setupRoutes() {
     this.router.use("/events", this.eventRoutes.getRouter());
+    this.router.use("/auth", this.authRoutes.getRouter());
   }
 
   public getRouter() {
