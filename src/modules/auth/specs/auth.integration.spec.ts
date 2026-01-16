b30157bbd09d413538bc63f17ff7f7457d32a835
drummonds <drummond05@proton.me>
2026-01-16 16:44:54 -0300

    feat: auth intergration tests for login and register

---
+++ src/modules/auth/specs/auth.integration.spec.ts
@@ -0,0 +1,82 @@
+import { App } from "../../../app";
+import { describe, expect, it } from "vitest";
+import request from "supertest";
+import { container } from "tsyringe";
+
+const req = request(container.resolve(App).express);
+const BASE_URL = "/api/v1/auth";
+
+async function registerNewUser(override?: Record<string, unknown>) {
+  return await req.post(`${BASE_URL}/register`).send({
+    email: "test@test.com",
+    name: "testUser",
+    password: "12345678",
+    confirmPassword: "12345678",
+    ...override,
+  });
+}
+
+describe("POST /api/v1/auth/register", () => {
+  it("should return status 201 and new user with valid body", async () => {
+    const result = await registerNewUser();
+
+    expect(result.status).toBe(201);
+    expect(result.body).toMatchObject({
+      email: expect.any(String),
+      name: expect.any(String),
+      role: "USER",
+      created_at: expect.any(String),
+    });
+  });
+
+  it("should return status 409 when send duplicated email", async () => {
+    await registerNewUser();
+    const result = await registerNewUser();
+
+    expect(result.status).toBe(409);
+    expect(result.body).toHaveProperty("message");
+  });
+
+  it("should return status 400 when send extra fields", async () => {
+    const result = await registerNewUser({
+      role: "ADMIN",
+    });
+
+    expect(result.status).toBe(400);
+    expect(result.body).toHaveProperty("message");
+    expect(result.body).toHaveProperty("details");
+  });
+});
+
+describe("POST /api/v1/auth/login", () => {
+  it("should return status 204 with cookie http only when credentials are valid", async () => {
+    const credentials = {
+      email: "test@valid.com",
+      password: "12345678",
+    };
+    await registerNewUser(credentials);
+
+    const result = await req.post(`${BASE_URL}/login`).send(credentials);
+    const cookie = result.headers["set-cookie"]?.[0].toLowerCase() ?? "";
+
+    expect(result.status).toBe(204);
+    expect(result.body).toEqual({});
+    expect(cookie).toContain("sid");
+    expect(cookie).toContain("httponly");
+    expect(cookie).toContain("secure");
+    expect(cookie).toContain("samesite=lax");
+  });
+  
+  it("should return status 401  when credentials are invalid", async () => {
+    const credentials = {
+      email: "test@invalid.com",
+      password: "12345678",
+    };
+    await registerNewUser();
+
+    const result = await req.post(`${BASE_URL}/login`).send(credentials);
+
+    expect(result.status).toBe(401);
+    expect(result.body).toHaveProperty("message")
+  });
+});
