c994a6b81f3d7adcef3d11cab318cd13be65556d
drummonds <drummond05@proton.me>
2026-01-17 12:14:58 -0300

    feat: Add `owner_id` column to the `events` table with a foreign key constraint to the `users` table.

---
+++ src/database/migrations/1768662296513_add-ownerid-to-events-table.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.addColumn("events", {
+        owner_id: {
+            type: "UUID",
+            notNull: true,
+            references: "users(id)",
+            onDelete: "CASCADE",
+        }
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropColumn("events", "owner_id");
+};
