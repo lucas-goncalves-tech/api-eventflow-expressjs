3672ca80a61fc4011cdec7300eaf75e228d367ce
Drummonds <drummond05@proton.me>
2026-01-21 21:31:08 -0300

    feat(event): integration events test on POST method

--- src/modules/events/specs/event.integration.spec.ts
+++ src/modules/events/specs/event.integration.spec.ts
@@ -18,8 +18,8 @@ const BASE_URL = "/api/v1/events";
 const req = request(container.resolve(App).express);
 
 async function postEvent(
-  agent: InstanceType<typeof TestAgent>,
-  override?: CreateEventDTO,
+  agent: TestAgent,
+  override?: CreateEventDTO | Record<string, unknown>,
 ) {
   const payload = generateNewEvent(override);
   const result = await agent.post(BASE_URL).send(payload);
@@ -27,10 +27,10 @@ async function postEvent(
   return { payload, result };
 }
 
-describe(`GET ${BASE_URL}`, () => {
-  it("should create event when role is ORGANIZER or ADMIN", async () => {
+describe(`POST ${BASE_URL}`, () => {
+  it("should create event when role is ORGANIZER", async () => {
     const { reqAgent } = await authUser("organizer");
-    const { result, payload } = await postEvent(reqAgent);
+    const { result } = await postEvent(reqAgent);
     const body = result.body as ICreateEventResponse;
     const shape = expectedEventShape();
 
@@ -39,12 +39,52 @@ describe(`GET ${BASE_URL}`, () => {
     expect(body.data).toMatchObject(shape);
     expect(body.data).not.toHaveProperty("owner_id");
   });
+  it("should create event when role is ADMIN", async () => {
+    const { reqAgent } = await authUser("admin");
+    const { result } = await postEvent(reqAgent);
 
-  it("should return 201 when role is USER", async () => {
+    expect(result.status).toBe(201);
+  });
+
+  it("should return status 403 when role is USER", async () => {
     const { reqAgent } = await authUser("user");
     const { result } = await postEvent(reqAgent);
 
     expect(result.status).toBe(403);
     expect(result.body).toHaveProperty("message");
   });
+
+  it("should return status 400 when body is invalid", async () => {
+    const { reqAgent } = await authUser("organizer");
+    const { result } = await postEvent(reqAgent, {
+      banana: "wrog value",
+    });
+
+    expect(result.status).toBe(400);
+    expect(result.body).toHaveProperty("message");
+  });
+
+  it("should return status 400 when ends_at less than starts_at", async () => {
+    const now = new Date();
+    let ends_at = now;
+    ends_at.setDate(now.getDate() - 1);
+
+    const { reqAgent } = await authUser("organizer");
+    const { result } = await postEvent(reqAgent, {
+      starts_at: now,
+      ends_at,
+    });
+
+    expect(result.status).toBe(400);
+    expect(result.body).toHaveProperty("message");
+  });
+
+  it("should return status 401 when access_token not exist", async () => {
+    const { result } = await postEvent(req);
+
+    expect(result.status).toBe(401);
+    expect(result.body).toHaveProperty("message");
+  });
 });
+
+describe(`GET ${BASE_URL}`, () => {});
